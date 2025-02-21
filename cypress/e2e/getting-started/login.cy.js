f9349d3d1287592cf501b0f284a6f7ee2e18f78a
ikalathifah <ikalathifah24@gmail.com>
2025-02-21 08:29:57 +0700

    test-case terakhir

--- cypress/e2e/getting-started/login.cy.js
+++ cypress/e2e/getting-started/login.cy.js
@@ -1,7 +1,7 @@
 import loginPage from "../pomMelaka/login";
 
 describe('Fitur Login', () => {
-    it('Test Function Login with Valid Credentials', () => {
+    it('Test Function Login with Valid Credentials to Account Activation', () => {
         cy.visit('https://dashboard.melaka.app/register');
         cy.get('[class="body-b2 text-le text-center"]').should('have.text', 'Sudah punya akun? Masuk disini.');
         loginPage.buttonMasukRegister().click();
@@ -13,4 +13,37 @@ describe('Fitur Login', () => {
         loginPage.aktivasiAkun().should('have.text', 'Aktivasi Akun');
     }
     )
-  })
+})
+
+describe('Fitur Login', () => {
+    it('Test Function Login with Valid Credentials', () => {
+        cy.visit('https://dashboard.melaka.app/register');
+        cy.get('[class="body-b2 text-le text-center"]').should('have.text', 'Sudah punya akun? Masuk disini.');
+        loginPage.buttonMasukRegister().click();
+        cy.get('[class="title-large"]').should('have.text', 'Login');
+        loginPage.inputEmail().type('devenryn@gmail.com');
+        loginPage.inputPassword().type('deven12345');
+        loginPage.buttonLogin().click();
+        cy.wait(2000);
+        cy.get('[class="slidedown-body-message"]').should('have.text', 'Aktifkan notifikasi untuk terus update pesanan baru dan pemberitahuan lainnya!');
+        cy.get('[id="onesignal-slidedown-cancel-button"]').click();
+        cy.get('[class="inline-flex w-full items-center gap-2"]').eq(0).should('have.text', 'Beranda');
+    }
+    )
+})
+
+describe('Fitur Login', () => {
+    it('Test Function Login with Invalid Credentials', () => {
+        cy.visit('https://dashboard.melaka.app/register');
+        cy.get('[class="body-b2 text-le text-center"]').should('have.text', 'Sudah punya akun? Masuk disini.');
+        loginPage.buttonMasukRegister().click();
+        cy.get('[class="title-large"]').should('have.text', 'Login');
+        loginPage.inputEmail().click();
+        loginPage.inputPassword().click();
+        loginPage.buttonLogin().click();
+        cy.intercept("GET", "**/employees/action-summary").as("actionsummary");
+        cy.get('[data-testid="login__field__email__error"]').should('have.text', 'Wajib diisi.');
+        cy.get('[data-testid="login__field__password__error"]').should('have.text', 'Wajib diisi.');
+    }
+    )
+})
--- cypress/e2e/getting-started/register.cy.js
+++ cypress/e2e/getting-started/register.cy.js
@@ -33,9 +33,8 @@ describe('Fitur Register', () => {
       registerPage.confirmPassword().type('deven12345');
       registerPage.checkboxButton().click();
       registerPage.daftarButton().click();
-      cy.wait(2000);
-      cy.get('[class="text-[1.125rem] md:text-[1.375rem] font-bold leading-[1.75rem] md:leading-[1.75rem] text-he mb-6"]').should('have.text', 'Akunmu selesai dibuat!');
-      cy.get('[data-testid="register-complete__sticky-box-text__verification__title"]').should('have.text','Link verifikasi telah dikirim ke email.');
+      cy.get('[data-testid="register__text-field__phone-number__error"]').should('have.text', 'Maaf, identitas sudah digunakan. Silakan coba yang lain.');
+      cy.get('[data-testid="register__text-field__email__error"]').should('have.text', 'Maaf, identitas sudah digunakan. Silakan coba yang lain.')
   }
   )
 })
@@ -46,6 +45,7 @@ describe('Fitur Register', () => {
       cy.get('[class="title-large text-he"]').should('have.text', 'Selamat Datang di Melaka');
       registerPage.inputNama().type('Ryn');
       registerPage.inputNomorTelepon().type('85169379718');
+      cy.get('[data-testid="register__text-field__name__error"]').should('have.text', 'Wajib diisi, min. 5 karakter.');
       registerPage.inputNamaBisnis().type('Kolam Renang');
       registerPage.buttonBrand().click();
       registerPage.inputEmail().type('devenryn@gmail.com');
@@ -64,9 +64,11 @@ describe('Fitur Register', () => {
       registerPage.inputNama().type('Deven Ryn');
       registerPage.inputNomorTelepon().click();
       registerPage.inputNamaBisnis().type('Kolam Renang');
+      cy.get('[data-testid="register__text-field__phone-number__error"]').should('have.text', 'Wajib diisi. Nomor telepon tidak boleh kurang dari 10 atau lebih dari 12 karakter.')
       registerPage.buttonBrand().click();
       registerPage.inputEmail().click();
       registerPage.inputPassword().type('deven12345');
+      cy.get('[data-testid="register__text-field__email__error"]').should('have.text', 'Wajib diisi.')
       registerPage.confirmPassword().type('deven12345');
       registerPage.checkboxButton().click();
       registerPage.daftarButton().click();
@@ -86,6 +88,7 @@ describe('Fitur Register', () => {
     registerPage.inputPassword().type('deven12345');
     registerPage.confirmPassword().type('deven67890');
     registerPage.checkboxButton().click();
+    cy.get('[data-testid="register__text-field__confirm-password__error"]').should('have.text', 'Belum sesuai dengan kata sandi.');
     registerPage.daftarButton().click();
   }
   )
